01ced24c335742b4835c609718ee37e621919c95
Koding <lexas@lexas.koding.io>
2016-02-21 13:31:51 -0600

    a sample component

--- public/js/index.js
+++ public/js/index.js
@@ -1,17 +1,8 @@
-(function(){
-    var $ = require('jquery');
-    var _ = require('underscore');
-    var BB = require('backbone');
-    
-    var MatchView = BB.View.extend({
-        id: 'matchView',
-        events: {},
-        initialize: function(){
-            this.listenTo(this.model, change, this.render);
-            
-        },
-        render: function(){}
-        
-    });
-    
-})();
+
+var $ = require('jquery');
+var _ = require('underscore');
+var BB = require('backbone');
+
+$(document).on('ready', function(){
+    require('./match');
+});
